2878696583d7d67528e0713797b88110d5dc3ea6
ViceAlt <vitor.landin@aluno.senai.br>
2025-09-24 16:23:35 -0300

    Update coin

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const coin = document.querySelector('.coin');
+let points = 0;
 {
     const jump = () => {
         mario.classList.add('jump');
@@ -8,10 +10,10 @@ const pipe = document.querySelector('.pipe');
         }, 500);
     };
     const loop = setInterval(() => {
-        console.log('loop');
         const pipePosition = pipe.offsetLeft;
-        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
-        
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+        const coinPosition = +window.getComputedStyle(coin).left.replace('px', '');
+
         if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
             pipe.style.animation = 'none';
@@ -24,6 +26,15 @@ const pipe = document.querySelector('.pipe');
             mario.style.width = '75px';
             mario.style.marginLeft = '50px';
             clearInterval(loop)
+        }
+
+        if(coinPosition <= 120 && marioPosition >= 120 && marioPosition < 180)
+        {
+        points++
+        mario.src = '../imagens/pipe.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        console.log('points')
         }
     }, 10);
     document.addEventListener('keydown', jump)
