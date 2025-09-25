fc7156ddbd6da27e0adafddd6e204205b2b945ab
ViceAlt <vitor.landin@aluno.senai.br>
2025-09-25 09:17:22 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ let points = 0;
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
         const coinPosition = +window.getComputedStyle(coin).left.replace('px', '');
+            coin.style.opacity= '100'
 
         if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
@@ -22,19 +23,18 @@ let points = 0;
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
 
+            coin.style.animation = 'none';
+            coin.style.left = `${coinPosition}px`
+
             mario.src = '../imagens/game-over.png';
-            mario.style.width = '75px';
+            mario.style.width = '65px';
             mario.style.marginLeft = '50px';
-            clearInterval(loop)
+            clearInterval(loop);
         }
 
         if(coinPosition <= 120 && marioPosition >= 120 && marioPosition < 180)
         {
-        points++
-        mario.src = '../imagens/pipe.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
-        console.log('points')
+            coin.style.opacity= '0'
         }
     }, 10);
     document.addEventListener('keydown', jump)
